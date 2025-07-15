64dabcc54f5eb21c83f558a45ac407eb1d4490ac
Prathamesh <prathamesh.appdid@gmail.com>
2025-07-15 19:07:08 +0530

    changes

--- lib/google_sign.dart
+++ lib/google_sign.dart
@@ -1,12 +1,13 @@
+import 'dart:developer';
+
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'auth_service.dart'; // Import AuthService to access user profile check
 
 final supabase = Supabase.instance.client;
 
 Future<AuthResponse> googleSignIn() async {
-  const webClientId =
-      '54391127653-s3nsu1769f7mqo8srdhdhm2r7fnqso1a.apps.googleusercontent.com';
+  log("in ");
+  const webClientId = '54391127653-s3nsu1769f7mqo8srdhdhm2r7fnqso1a.apps.googleusercontent.com';
 
   final GoogleSignIn googleSignIn = GoogleSignIn(
     serverClientId: webClientId,
@@ -16,36 +17,41 @@ Future<AuthResponse> googleSignIn() async {
 
   // Disconnect previous session to force account picker
   try {
-    await googleSignIn.disconnect();
+    if (googleSignIn.currentUser != null) {
+      await googleSignIn.disconnect();
+    }
   } catch (e) {
+    log("Erorr $e");
     print('Disconnect error (ignored): $e');
   }
-
-  final googleUser = await googleSignIn.signIn();
+  log("chekc ended");
+  GoogleSignInAccount? googleUser;
+ try {
+   googleUser = await googleSignIn.signIn();
+  log("googleUser: $googleUser");
   if (googleUser == null) {
+    log("Google Sign-In was cancelled.");
     throw Exception('Google Sign-In was cancelled.');
   }
-
+} catch (e) {
+  log("Google Sign-In failed: $e");
+  rethrow;
+}
+  log("message");
   final googleAuth = await googleUser.authentication;
   final accessToken = googleAuth.accessToken;
   final idToken = googleAuth.idToken;
-
+  log("$googleAuth      \n$accessToken \n $idToken");
   if (accessToken == null || idToken == null) {
-    throw Exception(
-        'Missing Google Auth tokens: accessToken=$accessToken, idToken=$idToken');
+    throw Exception('Missing Google Auth tokens: accessToken=$accessToken, idToken=$idToken');
   }
 
   // Check if the Google account's email is registered in the users table
   // final authService = AuthService();
-  final userProfile = await supabase
-      .from('users')
-      .select()
-      .eq('email', googleUser.email)
-      .maybeSingle();
-
+  final userProfile = await supabase.from('users').select().eq('email', googleUser.email).maybeSingle();
+  log("${userProfile?.entries.first}");
   if (userProfile == null) {
-    throw Exception(
-        'This Google account is not registered. Please sign up first.');
+    throw Exception('This Google account is not registered. Please sign up first.');
   }
 
   try {
